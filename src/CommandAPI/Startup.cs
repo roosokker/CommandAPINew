4b25114712eb247af27f6eadef5891f5349c78c7
rodaina sokkar <rodainasokkar@gmail.com>
2021-01-26 14:00:23 +0200

    deployment to azure

--- src/CommandAPI/Startup.cs
+++ src/CommandAPI/Startup.cs
@@ -44,7 +44,7 @@ namespace CommandAPI
             services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
             services.AddScoped<ICommandAPIRepo, SqlCommandAPIRepo>();
         }
-        /////fisjs
+        ///testtt
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env ,CommandContext context)
         {
